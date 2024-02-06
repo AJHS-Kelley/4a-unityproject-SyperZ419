bbc6eccd993b582062a04f3243d76afe97a9c204
SyperZ419 <142258433+SyperZ419@users.noreply.github.com>
2024-02-06 10:05:08 -0800

    Test Project

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        int myInt = 0;
+        float myFloat = 3.14F;
+        bool myBool = true;
+        string myString = "The FSCJ field trip was boring.\n";
+
+        Console.WriteLine("My Integer variable is " + myInt);
+        Console.WriteLine("My Float variable is " + myFloat);
+        Console.WriteLine("My Boolean variable is " + myBool);
+        Console.WriteLine("My String variable is " + myString);
+
+        for(int i = 0; i <= 20; i++){
+            Console.WriteLine("I ate " + i + " slices of pizza for lunch.\n");
+        }
     }
 }
